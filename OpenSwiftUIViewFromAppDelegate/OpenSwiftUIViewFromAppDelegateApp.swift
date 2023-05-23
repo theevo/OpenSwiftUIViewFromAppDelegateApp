6b1452b3163e05d0e62572f23ce42ab0f0b02904
Theo Vora <theo.vora@gmail.com>
2023-05-23 12:10:38 -0500

    Initial Commit

---
+++ OpenSwiftUIViewFromAppDelegate/OpenSwiftUIViewFromAppDelegateApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSwiftUIViewFromAppDelegateApp.swift
+//  OpenSwiftUIViewFromAppDelegate
+//
+//  Created by Theo Vora on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenSwiftUIViewFromAppDelegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
