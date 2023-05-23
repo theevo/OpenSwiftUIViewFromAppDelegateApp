fc6652249dc6961e36e28bf639f3678d852d013a
Theo Vora <theo.vora@gmail.com>
2023-05-23 15:42:02 -0500

    add breadcrumbs

--- OpenSwiftUIViewFromAppDelegate/AppleColorView.swift
+++ OpenSwiftUIViewFromAppDelegate/AppleColorView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct AppleColorView: View {
-    @State private var path: [Color] = [] // Nothing on the stack by default.
-    // @State private var path: [Color] = [.green, .mint] // try it with this
+//    @State private var path: [Color] = [] // Nothing on the stack by default.
+     @State private var path: [Color] = [.green, .mint] // try it with this
 
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                NavigationLink("Purple", value: Color.purple)
-                NavigationLink("Pink", value: Color.pink)
-                NavigationLink("Orange", value: Color.orange)
+            VStack {
+                Text("Insert anything here")
+                    .font(.italic(.body)())
+                Text("Here's what really matters:")
+                    .font(.largeTitle)
+                Text("Feed the $path")
             }
             .navigationDestination(for: Color.self) { color in
                 ColorDetail(color: color)
