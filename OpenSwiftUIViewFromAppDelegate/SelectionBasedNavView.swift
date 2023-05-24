ece8c229d68ce1bcdd499a27a60330d976fc6f30
Theo Vora <theo.vora@gmail.com>
2023-05-24 14:38:52 -0500

    add Selection Based NavigationView

---
+++ OpenSwiftUIViewFromAppDelegate/SelectionBasedNavView.swift
@@ -0,0 +1,40 @@
+//
+//  SelectionBasedNavView.swift
+//  OpenSwiftUIViewFromAppDelegate
+//
+//  Created by Theo Vora on 5/23/23.
+//
+
+import SwiftUI
+
+struct SelectionBasedNavView: View {
+    let colors: [Color] = [.purple, .pink, .orange]
+    @Binding var selection: Color? // Nothing selected by default.
+
+    var body: some View {
+        NavigationSplitView {
+            List(colors, id: \.self, selection: $selection) { color in
+                NavigationLink(color.description, value: color)
+            }
+        } detail: {
+            if let color = selection {
+                ColorDetail(color: color)
+            } else {
+                Text("Pick a color")
+            }
+        }
+    }
+}
+
+struct SelectionBasedNavView_Previews: PreviewProvider {
+    struct PinkSelectedPreview: View {
+        @State var color: Color = Color.pink
+        var body: some View {
+            SelectionBasedNavView(selection: .constant(color))
+        }
+    }
+    
+    static var previews: some View {
+        PinkSelectedPreview()
+    }
+}
