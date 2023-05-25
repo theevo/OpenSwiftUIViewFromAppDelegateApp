309b1fa3486fc2796953407882baa1a8bc61da1b
Theo Vora <theo.vora@gmail.com>
2023-05-25 10:23:56 -0500

    compose a view with a NavigationStack

---
+++ OpenSwiftUIViewFromAppDelegate/MyNavView.swift
@@ -0,0 +1,27 @@
+//
+//  MyNavView.swift
+//  OpenSwiftUIViewFromAppDelegate
+//
+//  Created by Theo Vora on 5/25/23.
+//
+
+import SwiftUI
+
+struct MyNavView: View {
+    let numbers: [Int] = Array(1...25)
+    
+    var body: some View {
+        NavigationStack {
+            List(numbers, id: \.self, rowContent: { num in
+                NavigationLink("Row \(num)", value: num)
+            })
+            .navigationTitle("A bunch of numbers")
+        }
+    }
+}
+
+struct MyNavView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyNavView()
+    }
+}
